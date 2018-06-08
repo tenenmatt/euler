ac3554d444148037c44dd8b297bf6c275bef9768
Matt Tenenbaum <matt.tenenbaum@rockyou.com>
2018-06-07 20:25:18 -0700

    First couple problems

---
+++ euler/problem_1.py
@@ -0,0 +1,27 @@
+"""
+Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or
+5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+"""
+
+
+import itertools as it
+
+
+def multiples_of(n):
+    return (n * i for i in it.count(1))
+
+
+def upto(seq, threshold):
+    return it.takewhile(lambda x: x < threshold, seq)
+
+
+# Actual problem-statement is summed_multiples([3, 5], 1000)
+def summed_multiples(bases, threshold):
+    return sum(sum(upto(multiples_of(b), threshold))
+               for b in bases)
+
