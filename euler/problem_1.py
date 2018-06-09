fed9e6a023454d0436a11c2f771781bc33bd5001
Matt Tenenbaum <matt.tenenbaum@rockyou.com>
2018-06-08 17:30:32 -0700

    Fix bug in p1 (double-counting shared factors)

--- euler/problem_1.py
+++ euler/problem_1.py
@@ -10,6 +10,9 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 
 import itertools as it
+from functools import reduce
+
+import pdb
 
 
 def multiples_of(n):
@@ -22,6 +25,9 @@ def upto(seq, threshold):
 
 # Actual problem-statement is summed_multiples([3, 5], 1000)
 def summed_multiples(bases, threshold):
-    return sum(sum(upto(multiples_of(b), threshold))
-               for b in bases)
-
+    mults = [set(upto(multiples_of(b), threshold))
+             for b in bases]
+    # sum the union of all those sets (avoid duplicates, eg multiples
+    # of 15 when including 3 and 5 in bases).
+    # 'reduce' here is just a terse way to take the union of all sets
+    return sum(reduce(lambda x, y: x.union(y), mults))
