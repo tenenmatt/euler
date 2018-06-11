edb9ed3d3647f0ef3f11457655932699564edd70
Matt Tenenbaum <matt.tenenbaum@rockyou.com>
2018-06-10 23:39:54 -0700

    Use segmented sieve

--- euler/problem_3.py
+++ euler/problem_3.py
@@ -1,16 +1,22 @@
 import itertools as it
 import numpy as np
+import pdb
 
 
-def factors(n):
-    #return (p for p in range(2, n) if is_prime(p) and n % p == 0)
-    return (p for p in sieve(n) if n % p == 0)
+# TODO: With large values of n, sieve is pretty inefficient
+# (even with segmentation). Better to just iterate over factors?
+
+
+def bool_array(n):
+    """Array of bool flags, length n"""
+    return np.full(n, True, dtype=bool)
 
 
 # Who doesn't love the Sieve of Eratosthenes?
 def sieve(n):
+    """Primes p < n"""
     assert n > 1
-    flags = np.full(n, True, dtype=bool)
+    flags = bool_array(n)
     for i in range(2, 1+int(np.sqrt(n))):
         if flags[i]:
             for j in it.takewhile(lambda x: x < n,
@@ -19,9 +25,38 @@ def sieve(n):
     return (i for i in range(2, n) if flags[i])
 
 
+def multiples_between(n, low, high):
+    if n <= low <= high:
+        base = np.ceil(low / n)
+        return it.takewhile(lambda x: x < high,
+                            (int(n * i) for i in it.count(base)))
+    return []
+
+
 def segmented_sieve(n, delta=None):
     if delta is None:
         delta = int(np.sqrt(n))
-    low_segment = sieve(delta) # primes in the first segment
+    # Find primes in first segment
+    primes = list(sieve(delta))
     # now find primes for other segments
-    pass
+    seg_low, seg_high = 0, delta
+    while seg_high < n:
+        # Increment segment (but truncate at n)
+        seg_low, seg_high = seg_high, min(n, seg_high + delta)
+        seg_size = seg_high - seg_low # hence we need adaptive sizing
+        # Initialize flags for this segment
+        seg = bool_array(seg_size)
+        # all primes up to this segment
+        p_less = [p for p in primes if p < np.sqrt(seg_high)]
+        for p in p_less:
+            # update flags for multiples of p in [seg_low, seg_high)
+            for m in multiples_between(p, seg_low, seg_high):
+                seg[m - seg_low] = False
+        primes.extend(i + seg_low for i in range(seg_size) if seg[i])
+    return primes
+
+
+def factors(n):
+    #return (p for p in range(2, n) if is_prime(p) and n % p == 0)
+    return (p for p in sieve(n) if n % p == 0)
+
--- tests/test_problem_3.py
+++ tests/test_problem_3.py
@@ -1,9 +1,23 @@
 import pytest
 
-from euler.problem_3 import is_prime, factors
+from euler.problem_3 import factors, segmented_sieve, multiples_between
 
 
 def test_factors():
     assert {2, 5} == set(factors(10))
     assert {2, 5} == set(factors(20))
     assert {3, 7} == set(factors(63))
+
+
+def test_multiples_between():
+    assert [4, 6] == list(multiples_between(2, 3, 7))
+    # includes bounds as expected
+    assert [4, 6] == list(multiples_between(2, 4, 8))
+
+
+#@pytest.mark.skip(reason="working out multiples_between")
+def test_segmented_sieve():
+    assert [2, 3, 5, 7] == segmented_sieve(10)
+    assert [2, 3, 5, 7, 11, 13] == segmented_sieve(16)
+    # top-most value is excluded
+    assert [2, 3, 5, 7, 11] == segmented_sieve(13)
