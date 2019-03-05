b776251a87b23940604756738afee50c0ed72b86
Matt Tenenbaum <matt.tenenbaum@rockyou.com>
2019-03-04 22:19:59 -0800

    Solution to Problem 18 (path sum part 1)
    We can carefully accumulate values to simplify this problem.

--- euler/problem_18.py
+++ euler/problem_18.py
@@ -34,10 +34,25 @@ solved by brute force, and requires a clever method! ;o)
 
 """
 
-
-from collections import namedtuple
-
-Node = namedtuple('Node', 'value, left, right')
+# A potential clever trick: once we parse the initial triangle, cumulate
+# the valuses:
+# 
+#                                   A
+#                                  B C
+#                                 D E F
+# 
+# becomes
+# 
+#                                   A
+#                                A+B A+C
+#                        A+B+D max(A+B+E, A+C+E) A+C+F
+# 
+# Then the max value is just the largest leaf. No tree traversal, no
+# clever path algorithms.
+#
+# Note that the inner values must take the max of their two parent-sums.
+# We can get away with this because the larger value represents the path
+# we would choose to take when we're summing terms.
 
 
 def parse_triangle(tri_str):
@@ -46,36 +61,47 @@ def parse_triangle(tri_str):
     return rows
 
 
-def make_tree(rows):
-    def crown(tree_rows):
-        return tree_rows[0][0]
-
-    if len(rows) == 1:
-        return Node(crown(rows), None, None)
-    else:
-        return Node(crown(rows),
-                    # left subtree
-                    make_tree([r[:-1] for r in rows[1:]]),
-                    # right subtree
-                    make_tree([r[1:] for r in rows[1:]]))
-
-
-def is_leaf(node):
-    return node.left is None and node.right is None
-
-
-# Always take the largest next option. This won't always work!
-def max_path_sum(tree):
-    node_value = tree.value
-    if is_leaf(tree):
-        return node_value
-    elif tree.left > tree.right:
-        return node_value + max_path_sum(tree.left)
-    else:
-        return node_value + max_path_sum(tree.right)
+def cumulate_triangle(tri):
+    assert len(tri) > 0
+    res = [tri[0]]
+    for i, row in enumerate(tri[1:]):
+        prev_row = res[i]
+        # r[0] increments r-1[0]; r[-1] increments r-1[-1],
+        # everything in between ads max(r-1[i-1] and r-1[i])
+        first = row[0] + prev_row[0]
+        last = row[-1] + prev_row[-1]
+        inner = []
+        if len(row) > 2:
+            inner = [row[j] + max(prev_row[j-1], prev_row[j])
+                     for j in range(1, len(row)-1)]
+        res.append([first] + inner + [last])
+    return res
 
 
-def paths(tree):
-    pass
+def max_path_sum(tri):
+    cumul = cumulate_triangle(tri)
+    # greatest path sum is just the largest leaf value in the last row
+    return max(cumul[-1])
 
 
+
+if __name__ == '__main__':
+    PROBLEM_INPUT = '''
+                                  75
+                                95 64
+                               17 47 82
+                             18 35 87 10
+                            20 04 82 47 65
+                          19 01 23 75 03 34
+                         88 02 77 73 07 63 67
+                       99 65 04 28 06 16 70 92
+                      41 41 26 56 83 40 80 70 33
+                    41 48 72 33 47 32 37 16 94 29
+                   53 71 44 65 25 43 91 52 97 51 14
+                 70 11 33 28 77 73 17 78 39 68 17 57
+                91 71 52 38 17 14 91 43 58 50 27 29 48
+              63 66 04 68 89 53 67 30 73 16 69 87 40 31
+             04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+'''
+    answer = max_path_sum(parse_triangle(PROBLEM_INPUT))
+    print("Max path sum in problem triangle: {}".format(answer))
--- tests/test_problem_18.py
+++ tests/test_problem_18.py
@@ -3,42 +3,25 @@ import pytest
 from euler.problem_18 import *
 
 
-def test_make_tree():
-    small_tree = '''
+def test_cumulate_triangle():
+    small_tri = '''
                                   3
                                  7 4
 '''
-    # First, just a super-trivial single node
-    assert Node(3, None, None) == make_tree(parse_triangle('3'))
-    # Now lets work with the tiny tree above
-    expected = Node(3, Node(7, None, None), Node(4, None, None))
-    assert expected == make_tree(parse_triangle(small_tree))
-
-    med_tree = '''
+    assert [[3], [10, 7]] == cumulate_triangle(parse_triangle(small_tri))
+    med_tri = '''
                                   3
                                  7 4
                                 2 4 6
                                8 5 9 3
-    '''
-    expected = Node(3,
-                    Node(7,
-                         Node(2,
-                              Node(8, None, None),
-                              Node(5, None, None)),
-                         Node(4,
-                              Node(5, None, None),
-                              Node(9, None, None))),
-                    Node(4,
-                         Node(4,
-                              Node(5, None, None),
-                              Node(9, None, None)),
-                         Node(6,
-                              Node(9, None, None),
-                              Node(3, None, None))))
-    assert expected == make_tree(parse_triangle(med_tree))
+'''
+    expected = [[3],
+                [10, 7],
+                [12, 14, 13],
+                [20, 19, 23, 16]]
+    assert expected == cumulate_triangle(parse_triangle(med_tri))
 
 
-@pytest.mark.skip()
 def test_max_path_sum():
     small_tri = '''
                                   3
